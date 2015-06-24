962b764b23884916a309418a8707fc303c2e1902
付瑞伟 <18201189891@163.com>
2015-06-24 10:34:52 +0800

    添加列表组件

--- js/publicjs/common.js
+++ js/publicjs/common.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-	$("li").click(function() {
-		$(this).siblings().removeClass("test");
-		$(this).addClass("test");
+	var navLabel = new Array('简要介绍', '文档', '列表', "地图", '饼图', '饼图', '漏斗图', '图片滑动', '列表滑动', "下拉框", '模态窗口', '分页', '折线图', '导航', "按钮", '雷达图');
+	$('#main').pageScroller({
+		navigation: navLabel
 	});
-	
-	
 });
+
+$(document).ready(function(){
+	$(".listSampleGraph").load("sourceCode/list.html"); 
+	$(".listsourceCode").load("sourceCode/list.html"); 
+}); 
---
+++ js/publicjs/loadjscssfile.js
@@ -0,0 +1,20 @@
+// js css 动态加载器
+function loadjscssfile(filename, filetype) {
+
+	if (filetype == "js") {
+		var fileref = document.createElement('script');
+		fileref.setAttribute("src", filename);
+
+	} else if (filetype == "css") {
+		var fileref = document.createElement('link');
+		fileref.setAttribute("rel", "stylesheet");
+		fileref.setAttribute("href", filename);
+	}
+	if (typeof fileref != "undefined") {
+		document.getElementsByTagName("head")[0].appendChild(fileref);
+	}
+}
+
+loadjscssfile("css/referencecss/bootstrap.min.css","css");
+loadjscssfile("css/publiccss/base.css","css");
+loadjscssfile("css/library.css","css");
